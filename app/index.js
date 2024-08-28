2790a3889f09333a93867d6526dd5618438f9c9b
Jonatan <Jonatan_Arroyo@outlook.com>
2024-08-28 13:31:09 -0300

    Creacion del Tablero

--- app/index.js
+++ app/index.js
@@ -1,31 +1,55 @@
-const d =document;
+const d = document;
 const root = d.getElementById('root');
 const img = "agregar piezas";
 
+const checkCoord = (row, col) => (row % 2 === 0 && col % 2 === 0) || (row % 2 === 1 && col % 2 === 1);
+
 class Board {
-    board = [];
-    constructor(cols, rows){
+    constructor(cols, rows) {
         this.cols = cols;
         this.rows = rows;
+        this.board = [];
     }
-    getBoard(){
-        for(let i=0; i< this.rows; i++){
-            for(let j=0; j< this.cols; j++){
-                switch(true){
-                case i< 3: {
-                    if(i%2===0 && j%2===0 || i%2===1 && j%2===1){
-                        this.board.push = "dark"};
-                        
+
+    getBoard() {
+        for (let i = 0; i < this.rows; i++) {
+            let row = [];
+            for (let j = 0; j < this.cols; j++) {
+                if((i+j)%2 === 0){
+                    checkCoord(i, j) ? row.push("dark") : row.push(null);
+                } else{
+                    checkCoord(i, j) ? row.push("white") : row.push(null);
                 }
-                case (i>4): {
-                    if(i%2===0 && j%2===0 || i%2===1 && j%2===1){
-                        this.board.push = "white"};
-                        break;
-                        
-                }
-                default: this.board.push(null); 
             }
+            this.board.push(row);
         }
     }
+    createBoard(element){
+        const tablero = d.createElement('div');
+        tablero.setAttribute("class", "tablero");
+        tablero.style.display = 'grid';
+        tablero.style.gridTemplateColumns = `repeat(${this.cols}, 1fr)`;
+        tablero.style.gridTemplateRows = `repeat(${this.rows}, 1fr)`;
+        
+        this.board.forEach((row,i) => {
+            row.forEach((cell, j) => {
+                const callElement = d.createElement('div');
+                callElement.style.width = '100px';
+                callElement.style.height = '100px';
+                if(cell === "dark") {
+                    callElement.style.backgroundColor = 'hsl(30, 50%, 30%)';
+                } else{
+                    callElement.style.backgroundColor = 'hsl(30, 50%, 60%)';
+                }
+                tablero.appendChild(callElement);
+            });
+        });
+
+        element.appendChild(tablero);
+    }
 }
-}
+
+const game = new Board(8, 8);
+game.getBoard();
+console.log(root);
+game.createBoard(root);
